550cabc7530c6a5f4f5ea2fb2b7e09f9dba09f2d
Qocali <babayevqocheli@gmail.com>
2021-12-11 19:51:45 +0400

    Add project files.

---
+++ labb2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace labb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person() { name = "adil", age = 17, profession = "engeneer" };
+            Person person2 = new Person() { name = "sadiq", age = 23, profession = "devloper" };
+            Person person3 = new Person() { name = "sabir", age = 28, profession = "teacher" };
+            var list =new List<Person>() {person1,person2,person3 };
+            foreach (Person item in INSPECTIONS(list))
+            {
+                Console.WriteLine(item.ToString());
+            }
+            
+        }
+        static IEnumerable<Person> INSPECTIONS(List<Person> person)
+        {
+          
+           return person.Where(pres => pres.age >18);
+          
+        }
+    }
+
+    class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string profession { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name:{name}  Age:{age}  Profession:{profession}" ;
+        }
+    }
+}
